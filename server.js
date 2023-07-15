eb21c32d14018aff919d5458e0f3ce5d77b16b6e
wminaung <minwin243@gmail.com>
2023-07-15 21:19:34 +0630

    instal nodemon and add some code

--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const fs = require("fs");
 const server = http.createServer(function (req, res) {
   console.log("req.url", req.url, req.method);
 
-  if (req.url === "/home" && req.method == "GET") {
-    const html = fs.readFileSync("./index.html", { encoding: "utf-8" });
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(html);
-    return res.end();
+  const url = req.url;
+  const method = req.method;
+
+  if (method === "GET") {
+    if (url === "/home") {
+      // const html = fs.readFileSync("./index.html", { encoding: "utf-8" });
+      // res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("oksdfsdfsfd");
+      return res.end();
+    } else if (url === "/style.css") {
+      const css = fs.readFileSync("./style.css", { encoding: "utf-8" });
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(css);
+      return res.end();
+    } else if (url === "/script.js") {
+      const script = fs.readFileSync("./script.css", { encoding: "utf-8" });
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(script);
+      return res.end();
+    }
   }
 
   res.end();
